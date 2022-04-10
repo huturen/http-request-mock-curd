26106a9a9a57c4afd850c4035359a77e03debef9
hushuilong <364346488@qq.com>
2022-04-10 14:49:23 +0800

    arco-cli: initialize project

---
+++ src/router/index.ts
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import NProgress from 'nprogress'; // progress bar
+import 'nprogress/nprogress.css';
+
+import DefaultLayout from '@/layout/default-layout.vue';
+import Login from './routes/modules/login';
+import appRoutes from './routes';
+import createRouteGuard from './guard';
+
+NProgress.configure({ showSpinner: false }); // NProgress Configuration
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: 'login',
+    },
+    Login,
+    {
+      name: 'root',
+      path: '/',
+      component: DefaultLayout,
+      children: appRoutes,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('@/views/not-found/index.vue'),
+    },
+  ],
+  scrollBehavior() {
+    return { top: 0 };
+  },
+});
+
+createRouteGuard(router);
+
+export default router;
---
+++ src/router/routes/modules/dashboard.ts
@@ -0,0 +1,23 @@
+export default {
+  path: 'dashboard',
+  name: 'dashboard',
+  component: () => import('@/views/dashboard/index.vue'),
+  meta: {
+    locale: 'menu.dashboard',
+    requiresAuth: true,
+    icon: 'icon-dashboard',
+    order: 0,
+  },
+  children: [
+    {
+      path: 'workplace',
+      name: 'Workplace',
+      component: () => import('@/views/dashboard/workplace/index.vue'),
+      meta: {
+        locale: 'menu.dashboard.workplace',
+        requiresAuth: true,
+        roles: ['*'],
+      },
+    },
+  ],
+};
