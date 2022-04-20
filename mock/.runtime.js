298fcd61f03433b4a86cb437ee988b9d6bf88483
hushuilong <364346488@qq.com>
2022-04-21 04:11:03 +0800

    chore: update package.json

--- mock/.runtime.js
+++ mock/.runtime.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 let mocker;
-import HttpRequestMock from 'http-request-mock/http-request-mock.esm.mjs';
+import HttpRequestMock from 'http-request-mock';
 
 mocker = HttpRequestMock.setup();
 
