cb9bf0a17f5950484b23585b11b3cddefb37d528
hushuilong <364346488@qq.com>
2022-05-13 10:37:22 +0800

    chore: update introduction.vue

--- src/views/dashboard/components/introduction.vue
+++ src/views/dashboard/components/introduction.vue
@@ -5,7 +5,7 @@
     即使在没有后台接口的情况下，来实现产品研发的闭环。 （F12查看调试工具中的日志。）
     <br />
     <br />
-    For CRUD operations of the user list below, no server requests will be actually generated. All modifications are
+    For CRUD operations of the user list below, no server requests will be generated. All change operations are
     implemented based on http-request-mock. <br />
     http-request-mock supports <a-typography-text mark>data-change-cache</a-typography-text>. You can use it to achieve
     a closed loop of product development. (Hit F12 to access Developer Tools and view the console logs.)
