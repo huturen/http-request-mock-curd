8033a56366d9497c17898ad5988c05aff32c68b2
shuilonghu <364346488@qq.com>
2024-09-07 18:00:35 +0800

    docs: update introduction

--- src/views/dashboard/components/introduction.vue
+++ src/views/dashboard/components/introduction.vue
@@ -4,7 +4,6 @@
     http-request-mock支持<a-typography-text mark>数据变更记忆</a-typography-text>， 你可以用此特性，
     即使在没有后台接口的情况下，来实现产品研发的闭环。 （F12查看调试工具中的日志。）
     <br />
-
     <br />
     For the CRUD operations on the user list below, no server requests will be initiated. All modifications are handled
     using http-request-mock. <br />
